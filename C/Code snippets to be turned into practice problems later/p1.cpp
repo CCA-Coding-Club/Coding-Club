7bbae25f17064cb83f4d9614c7d46e12bcafc051
westonmook <westonmook@gmail.com>
2025-11-08 07:59:56 -0700

    ReOrganization for C++ addition
    I moved all python specific directories under the new python folder and made a folder named C for its like purpose.

---
+++ C/Code snippets to be turned into practice problems later/p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    //This is a comment
+    /*
+        This is 
+        a multi
+        line
+        comment!
+    */
+    std::cout << "\nI like pizza!" << std::endl;
+    std::cout << "It's really good!" << "\n";
+    return 0;
+}
