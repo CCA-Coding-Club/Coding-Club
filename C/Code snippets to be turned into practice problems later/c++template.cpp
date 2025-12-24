0d8d89960a2fe2000b1fe01b37062fba568d11bf
westonmook <westonmook@gmail.com>
2025-12-24 11:35:38 -0700

    more c++
    template adjustment as well

--- C/Code snippets to be turned into practice problems later/c++template.cpp
+++ C/Code snippets to be turned into practice problems later/c++template.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+//#include <ctime>
 //template used
+
+//Here are some neat namespaces to consider using consistantly
+using std::cout;
+using std::string;
+using std::cin;
+using std::endl;
+//etc.
+
 int main() {
 
-    //Here are some neat namespaces to consider using consistantly
-    using std::cout;
-    using std::string;
-    using std::cin;
-    using std::endl;
-    //etc.
+
 
     std::cout << "\n";
 
